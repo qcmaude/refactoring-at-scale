a43ef450139202534ec2d0b53a59a6b94eec0f89
Maude Lemaire <maude.codes@gmail.com>
2020-07-05 00:12:31 -0700

    Now with JavaScript examples throughout rather than kicking off the book with a PHP example

---
+++ Chapter1/checkValid_v5.mjs
@@ -0,0 +1,51 @@
+import assert from 'assert';
+
+function checkValid(minimum, maximum, grades, useAverage = false) {
+
+  // Valid assignments should never allow fewer than 0 points
+  var absoluteMinimum = 0;
+
+  // Valid assignments should never exceed more than 100 possible points
+  var absoluteMaximum = 100;
+
+  if (minimum < absoluteMinimum) return false;
+  if (maximum > absoluteMaximum) return false;
+
+  var min = Math.min(...grades);
+  var max = Math.max(...grades);
+  if (useAverage) {
+    min = max = calculateAverage(grades);
+  }
+
+  if (min < minimum) return false;
+  if (max > maximum) return false;
+  return true;
+}
+
+function calculateAverage(grades) {
+  return grades.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+// Check for all values within bounds.
+var defaultSet = [10, 20, 30, 40, 50, 60];
+console.log("Expected true,", checkValid(0, 100, defaultSet));
+
+// Check for value below minimum (15).
+console.log("Expected false,", checkValid(15, 100, defaultSet));
+// Check for value above maximum (55).
+console.log("Expected false,", checkValid(0, 55, defaultSet));
+// Check for value at minimum (10).
+console.log("Expected false,", checkValid(10, 100, defaultSet));
+// Check for value at maximum (60).
+console.log("Expected false,", checkValid(0, 60, defaultSet));
+
+// Check for average within bounds.
+console.log("Expected true,", checkValid(0, 100, defaultSet));
+// Check for average below minimum.
+console.log("Expected false,", checkValid(40, 100, defaultSet));
+// Check for average at minimum.
+console.log("Expected false,", checkValid(35, 100, defaultSet));
+// Check for average above maximum.
+console.log("Expected false,", checkValid(0, 30, defaultSet));
+// Check for average at maximum.
+console.log("Expected false,", checkValid(0, 35, defaultSet));
